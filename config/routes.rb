5f7cca7e3ffa63769262fd917e93562aeab7be94
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-04-02 20:11:52 +0000

    added some stylying

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,31 @@
 Rails.application.routes.draw do
   # Routes for the Alert resource:
+  root "organizations#index"
 
-  # CREATE
-  post("/insert_alert", { :controller => "alerts", :action => "create" })
-          
-  # READ
-  get("/alerts", { :controller => "alerts", :action => "index" })
-  
-  get("/alerts/:path_id", { :controller => "alerts", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_alert/:path_id", { :controller => "alerts", :action => "update" })
-  
-  # DELETE
-  get("/delete_alert/:path_id", { :controller => "alerts", :action => "destroy" })
-
-  #------------------------------
+  devise_for :users
 
   resources :organizations
   resources :external_data
-  devise_for :users
-  root "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :alerts
+
+
+
+  # # CREATE
+  # post("/insert_alert", { :controller => "alerts", :action => "create" })
+          
+  # # READ
+  # get("/alerts", { :controller => "alerts", :action => "index" })
+  
+  # get("/alerts/:path_id", { :controller => "alerts", :action => "show" })
+  
+  # # UPDATE
+  
+  # post("/modify_alert/:path_id", { :controller => "alerts", :action => "update" })
+  
+  # # DELETE
+  # get("/delete_alert/:path_id", { :controller => "alerts", :action => "destroy" })
+
+  #------------------------------
+
+
 end
