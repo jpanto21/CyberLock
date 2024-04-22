f1f26c0085d7b750a1d42dc1a98c014c0d4f1bcf
Jose Pantoja <77706927+jpanto21@users.noreply.github.com>
2024-04-22 15:28:01 +0000

    testing api when deployed

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,27 +1,33 @@
-# app/controllers/api_controller.rb
-
 class ApiController < ApplicationController
   require 'net/http'
   require 'uri'
 
   def check_breaches
     email = params[:email]
-    api_key = ENV.fetch('HIBP_KEY', nil)
+    api_key = ENV.fetch('HIBP_KEY')
     uri = URI("https://haveibeenpwned.com/api/v3/breachedaccount/#{CGI.escape(email)}?truncateResponse=false")
 
     request = Net::HTTP::Get.new(uri)
     request['hibp-api-key'] = api_key
 
-    response = Net::HTTP.start(uri.hostname, uri.port,
-    use_ssl: uri.scheme == 'https') do |http|
-    http.request(request)
-    end
+    begin
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(request) }
 
-    if response.is_a?(Net::HTTPSuccess)
-      @breaches = JSON.parse(response.body)
-    else
+      case response
+      when Net::HTTPSuccess
+        @breaches = JSON.parse(response.body)
+      when Net::HTTPNotFound
+        @breaches = []
+        flash[:notice] = "No breaches found for this email."
+      else
+        @breaches = []
+        flash[:alert] = "Failed to retrieve breach data."
+        Rails.logger.error("Unexpected response from HIBP API: #{response.body}")
+      end
+    rescue StandardError => e
       @breaches = []
-      flash[:notice] = "No breaches found for this email."
+      flash[:alert] = "Error during API call: #{e.message}"
+      Rails.logger.error("Error during API call: #{e.message}")
     end
 
     render 'api/check_breaches'
